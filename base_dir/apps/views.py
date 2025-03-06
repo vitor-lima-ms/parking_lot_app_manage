8abdafd9318ee728f39455fd4dd9f4cece461d7a
Vitor Lima <viitorlima2000@gmail.com>
2025-03-06 20:06:33 -0300

    Update views.py

--- base_dir/apps/views.py
+++ base_dir/apps/views.py
@@ -1,7 +1,10 @@
 from django.views.generic.edit import FormView
 from .forms import ParkingSpaceCreationForm, AutosRegisterForm
-from .models import Autos, ParkingSpace
+from .models import ParkingSpace
 from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404
+from datetime import datetime, timezone
+from .functions import price_calculator
 
 # Create your views here.
 
@@ -19,19 +22,59 @@ def main_page(request):
         for i in range(form.cleaned_data['qtd']):
            ParkingSpace.objects.create()
 
-    all_parking_spaces = ParkingSpace.objects.all()
+    unoccupied_parking_spaces = ParkingSpace.objects.filter(occupied=False)
     
-    return render(request, 'main_page.html', {'all_parking_spaces': all_parking_spaces})
+    return render(request, 'main_page.html', {'unoccupied_parking_spaces': unoccupied_parking_spaces})
 
-
-class AutosFormView(FormView):
-    template_name = 'autos_form.html'
+"""View that allows create instances of Autos"""
+class AutosRegisterFormView(FormView):
+    template_name = 'autos_register_form.html'
     form_class = AutosRegisterForm
 
-def autos_register(request):
+"""View that allows assign autos to parking spaces"""
+def assign_autos_to_parking_space(request):
     form = AutosRegisterForm(request.POST)
-
     if form.is_valid():
-        autos = form.save()
+        auto = form.save()
+
+    unoccupied_parking_spaces = ParkingSpace.objects.filter(occupied=False)
+    parking_space = unoccupied_parking_spaces[0]
+
+    parking_space.add_auto(auto)
+    parking_space.save()
+
+    return redirect('apps:occupied_parking_spaces')
+
+"""Here we see all the occupied parking spaces"""
+def occupied_parking_spaces(request):
+    occupied_parking_spaces = ParkingSpace.objects.filter(occupied=True)
+
+    return render(request, 'occupied_parking_spaces.html', {'occupied_parking_spaces': occupied_parking_spaces})
+
+"""In this view, we calculate the amount to be paid and see all relevant information before receiving payment and releasing the parking space"""
+def pre_finish(request, parking_space_id):
+    parking_space = get_object_or_404(ParkingSpace, id=parking_space_id)
     
-    return render(request, 'autos_register.html', {'autos': autos})
+    checkin_datetime = parking_space.occupied_by.checkin_datetime
+    current_datetime = datetime.now(timezone.utc)
+
+    time_delta = current_datetime - checkin_datetime
+
+    price = round(price_calculator(time_delta), 2)
+
+    fmt_price = f'{price:.2f}'.replace('.', ',')
+
+    return render(request, 'pre_finish.html', {
+        'parking_space': parking_space,
+        'checkin_datetime': checkin_datetime,
+        'current_datetime': current_datetime,
+        'price': fmt_price,
+    })
+
+def finish(request, parking_space_id):
+    parking_space = get_object_or_404(ParkingSpace, id=parking_space_id)
+
+    parking_space.remove_auto()
+    parking_space.save()
+
+    return redirect('apps:main_page')
