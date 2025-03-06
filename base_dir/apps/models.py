b8e838434cbcf0db916d012dacf6c3f76fc91bbd
Vitor Lima <viitorlima2000@gmail.com>
2025-03-06 20:06:28 -0300

    Update models.py

--- base_dir/apps/models.py
+++ base_dir/apps/models.py
@@ -4,13 +4,13 @@ from django.db import models
 
 """Model representing automobiles"""
 class Autos(models.Model):
+    driver_name = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
     autos_plate = models.CharField(max_length=7)
-    checkin_date = models.DateField(auto_now_add=True)
-    checkin_time = models.TimeField(auto_now_add=True)
+    checkin_datetime = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = (('-checkin_time',))
+        ordering = (('-checkin_datetime',))
         verbose_name = 'Autos'
         verbose_name_plural = 'Autos'
     
@@ -30,3 +30,13 @@ class ParkingSpace(models.Model):
 
     def __str__(self):
         return f'Vaga {self.id}, {self.occupied}, {self.occupied_by}'
+    
+    """Function to add a veichle in a parking space"""
+    def add_auto(self, auto):
+        self.occupied = True
+        self.occupied_by = auto
+    
+    """Function to remove a veichle in a parking space"""
+    def remove_auto(self):
+        self.occupied = False
+        self.occupied_by = None
