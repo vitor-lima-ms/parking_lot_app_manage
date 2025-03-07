ebcc8bed0f51e01d3d2066b18da4434b1b5f62dd
Vitor Lima <viitorlima2000@gmail.com>
2025-03-07 01:30:12 -0300

    Update urls.py

--- base_dir/apps/urls.py
+++ base_dir/apps/urls.py
@@ -6,9 +6,19 @@ app_name = 'apps'
 urlpatterns = [
     path('parking_creation_form/', views.ParkingSpaceCreationFormView.as_view(), name='parking_creation_form'),
 
+    path('driver_register_form/', views.DriverRegisterFormView.as_view(), name='driver_register_form'),
+
+    path('saving_driver/', views.saving_driver, name='saving_driver'),
+
     path('autos_register_form/', views.AutosRegisterFormView.as_view(), name='autos_register_form'),
 
-    path('parking_assignment/', views.assign_autos_to_parking_space, name='parking_assignment'),
+    path('saving_autos/', views.saving_autos, name='saving_autos'),
+
+    path('parking_assignment/', views.ParkingAssignmentFormView.as_view(), name='parking_assignment'),
+
+    path('saving_parking_place/', views.saving_parking_place, name='saving_parking_place'),
+
+    path('registered_drivers/', views.registered_drivers, name='registered_drivers'),
 
     path('occupied_parking_spaces/', views.occupied_parking_spaces, name='occupied_parking_spaces'),
 
