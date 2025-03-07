b02e004f02dc785ee98decd724172f9c3b83c7cf
Vitor Lima <viitorlima2000@gmail.com>
2025-03-07 01:30:10 -0300

    Update models.py

--- base_dir/apps/models.py
+++ base_dir/apps/models.py
@@ -2,12 +2,28 @@ from django.db import models
 
 # Create your models here.
 
+"""Model representing drivers"""
+class Driver(models.Model):
+    driver_name = models.CharField(max_length=50)
+    monthly = models.BooleanField(default=False)
+    cnh = models.CharField(max_length=11)
+    cpf = models.CharField(max_length=11)
+    file_upload = models.FileField(upload_to='uploaded_files', blank=True)
+
+    def __str__(self):
+        return f'{self.driver_name}'
+
 """Model representing automobiles"""
 class Autos(models.Model):
-    driver_name = models.CharField(max_length=50)
+    driver_name = models.ForeignKey(
+        Driver,
+        on_delete=models.CASCADE,
+        default=None
+    )
     model = models.CharField(max_length=50)
     autos_plate = models.CharField(max_length=7)
     checkin_datetime = models.DateTimeField(auto_now_add=True)
+    parked = models.BooleanField(default=False)
 
     class Meta:
         ordering = (('-checkin_datetime',))
@@ -15,7 +31,7 @@ class Autos(models.Model):
         verbose_name_plural = 'Autos'
     
     def __str__(self):
-        return f'{self.model} {self.autos_plate}'
+        return f'{self.model}'
 
 """Model representing parking spaces"""
 class ParkingSpace(models.Model):
@@ -24,19 +40,15 @@ class ParkingSpace(models.Model):
         Autos,
         null=True,
         default=None,
-        on_delete=models.SET_NULL,
-        related_name='parking_space'        
+        on_delete=models.SET_NULL       
     )
+    history = models.JSONField(default=None, null=True)
 
     def __str__(self):
-        return f'Vaga {self.id}, {self.occupied}, {self.occupied_by}'
-    
-    """Function to add a veichle in a parking space"""
-    def add_auto(self, auto):
-        self.occupied = True
-        self.occupied_by = auto
+        return f'Vaga {self.id}'
     
     """Function to remove a veichle in a parking space"""
-    def remove_auto(self):
+    def remove_auto(self, auto):
+        
         self.occupied = False
         self.occupied_by = None
