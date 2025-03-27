6f5693aa24249ab7eddbe3145b582e6bf3835b2a
Vitor Lima <viitorlima2000@gmail.com>
2025-03-27 01:32:06 -0300

    Update urls.py

--- base_dir/apps/urls.py
+++ base_dir/apps/urls.py
@@ -6,6 +6,8 @@ app_name = 'apps'
 urlpatterns = [
     path('parking_creation_form/', views.ParkingSpaceCreationFormView.as_view(), name='parking_creation_form'),
 
+    path('parking_space_history/<int:parking_space_id>', views.parking_space_history, name='parking_space_history'),
+
     path('driver_register_form/', views.DriverRegisterFormView.as_view(), name='driver_register_form'),
 
     path('saving_driver/', views.saving_driver, name='saving_driver'),
@@ -18,13 +20,15 @@ urlpatterns = [
 
     path('saving_parking_place/', views.saving_parking_place, name='saving_parking_place'),
 
+    path('occupied_parking_spaces/', views.occupied_parking_spaces, name='occupied_parking_spaces'),
+
     path('registered_drivers/', views.registered_drivers, name='registered_drivers'),
 
-    path('occupied_parking_spaces/', views.occupied_parking_spaces, name='occupied_parking_spaces'),
+    path('registered_autos/', views.registered_autos, name='registered_autos'),
 
     path('pre_finish/<int:parking_space_id>', views.pre_finish, name='pre_finish'),
 
-    path('finish/<int:parking_space_id>', views.finish, name='finish'),
+    path('finish/<int:parking_space_id>/<int:autos_id>', views.finish, name='finish'),
     
     path('', views.main_page, name='main_page'),
 ]
